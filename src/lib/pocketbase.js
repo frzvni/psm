a218c1284b55cfaad82de3772fc7ada1602ade48
Rezvani <ft.rzvni@gmail.com>
2025-06-06 15:05:24 +0330

    add pocketbase lib

---
+++ src/lib/pocketbase.js
@@ -0,0 +1,7 @@
+import PocketBase from 'pocketbase';
+
+// Create a new PocketBase instance
+// Replace 'http://your-pocketbase-url' with your actual PocketBase server URL
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export default pb; 
